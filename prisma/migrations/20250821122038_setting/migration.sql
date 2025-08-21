45982a6102a93eb6dba1b0753cbef25d19d9e97e
navjotnetgains <navjot@netgains.org>
2025-08-21 17:50:56 +0530

    newsession

---
+++ prisma/migrations/20250821122038_setting/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "public"."Session" (
+    "id" TEXT NOT NULL,
+    "shop" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "isOnline" BOOLEAN NOT NULL DEFAULT false,
+    "scope" TEXT,
+    "expires" TIMESTAMP(3),
+    "accessToken" TEXT NOT NULL,
+    "userId" BIGINT,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "email" TEXT,
+    "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+    "locale" TEXT,
+    "collaborator" BOOLEAN DEFAULT false,
+    "emailVerified" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Setting" (
+    "shop" TEXT NOT NULL,
+    "doubleDraftOrdersEnabled" BOOLEAN NOT NULL DEFAULT false,
+    "discount1" DOUBLE PRECISION DEFAULT 0,
+    "discount2" DOUBLE PRECISION DEFAULT 0,
+    "tag1" TEXT DEFAULT '',
+    "tag2" TEXT DEFAULT '',
+    "singleDiscount" DOUBLE PRECISION DEFAULT 0,
+    "singleTag" TEXT DEFAULT '',
+
+    CONSTRAINT "Setting_pkey" PRIMARY KEY ("shop")
+);
